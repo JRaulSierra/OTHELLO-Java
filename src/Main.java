0938682c173e7d2a7f363d71bccca33feefe8ed7
JRaulSierra <j.raulsierra@gmail.com>
2022-10-17 20:29:59 -0600

    tabla vacia

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bienvenido al juego othello.");
+        Scanner sc = new Scanner(System.in);
+        int op = 0;
+        do {
+            System.out.println("Puede escojer una opciòn");
+            System.out.println("1. Iniciar juego\n2. Reglas\n3. Autor\n4. Salir");
+            op = sc.nextInt();
+            switch (op) {
+                case 1:
+                    table juego = new table();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    System.out.println("José Raúl Sierra Lòpez");
+                    System.out.println("Carnet: 22003703");
+                    break;
+                case 4:
+                    System.out.println("Saliendo");
+                    break;
+            }
+        } while (op != 4);
     }
 }
